f21bb427ab763be70768f515a9a2e8d7a085105d
Nikolaus Roman <nroman@e1z3r5p16.42.us.org>
2018-02-28 09:40:08 -0800

    fad

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: nroman <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 09:15:11 by nroman            #+#    #+#             */
-/*   Updated: 2018/02/28 09:16:01 by nroman           ###   ########.fr       */
+/*   Updated: 2018/02/28 09:39:05 by nroman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,11 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t		i;
 	size_t		j;
-	size_t		k;
-	size_t		n;
 
 	i = 0;
 	j = 0;
-	k = 0;
-	n = size;
 	while (dest[i] != '\0')
 		i++;
-	while (src[k] != '\0')
-		k++;
 	if (i <= size)
 	{
 		while (src[j] != '\0' && j < (size - i - 1))
@@ -35,10 +29,10 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 			j++;
 		}
 	}
-	if (size != 0 && i <= size)
+	if (size != 0 || i <= size)
 		dest[i + j] = '\0';
 	if (i > size)
-		return (size + k);
+		return (size + ft_strlen(src));
 	else
-		return (i + k);
+		return (i + ft_strlen(src));
 }
