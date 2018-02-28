a1abe51a58ba3d68a9e8e5d0fe5c3c235dd2906c
Nikolaus Roman <nroman@e1z3r5p16.42.us.org>
2018-02-27 20:52:12 -0800

    fdas

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -3,31 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memccpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nroman <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: lhurt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/19 11:27:21 by nroman            #+#    #+#             */
-/*   Updated: 2018/02/27 19:23:44 by nroman           ###   ########.fr       */
+/*   Created: 2016/09/24 11:56:21 by lhurt             #+#    #+#             */
+/*   Updated: 2016/09/24 11:56:23 by lhurt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memccpy(void *dest,
-		const void *src, int c, size_t len)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	size_t			i;
-	unsigned char	*d;
-	unsigned char	*s;
+	size_t	count;
+	char	*d;
+	char	*s;
 
-	d = (unsigned char *)dest;
-	s = (unsigned char *)src;
-	i = 0;
-	while ((i < len))
+	d = (char*)dst;
+	s = (char*)src;
+	count = 0;
+	while (count < n)
 	{
-		d[i] = s[i];
-		if (d[i] == (unsigned char)c)
-			return (dest + i + 1);
-		i++;
+		d[count] = s[count];
+		if (d[count] == c)
+			return ((void*)&d[count + 1]);
+		count++;
 	}
 	return (NULL);
 }
