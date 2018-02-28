4f3f7b0c618079e7d38b38509c19be472c5f750a
Nikolaus Roman <nroman@e1z3r5p16.42.us.org>
2018-02-27 20:50:03 -0800

    fdafd

--- ft_bzero.c
+++ ft_bzero.c
@@ -3,22 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nroman <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: lhurt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/19 10:57:42 by nroman            #+#    #+#             */
-/*   Updated: 2018/02/27 19:18:49 by nroman           ###   ########.fr       */
+/*   Created: 2016/09/21 16:02:37 by lhurt             #+#    #+#             */
+/*   Updated: 2016/09/21 16:02:38 by lhurt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *b, size_t len)
+void	ft_bzero(void *s, size_t n)
 {
-	size_t			i;
-	unsigned char	*cpy;
-
-	cpy = (unsigned char *)b;
-	i = 0;
-	while (i < len)
-		cpy[i++] = 0;
+	ft_memset(s, 0, n);
 }
